32258acbef2c73c938d5537548907db481a928f2
codewithmishra-ashish <work.mishraashish@gmail.com>
2025-01-12 21:16:47 +0530

    container update

--- index.php
+++ index.php
@@ -23,9 +23,9 @@
             background-color: #fff;
             border-radius: 10px;
             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-            padding: 30px;
+            padding: 20px;
             width: 100%;
-            max-width: 220px;
+            max-width: 250px;
         }
 
         .logo {
