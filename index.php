99f9b29a7558e1e6045bebc8451b558e072baea8
codewithmishra-ashish <work.mishraashish@gmail.com>
2025-01-15 19:27:37 +0530

    files update

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Campus Corner</title>
+    <title>myMANIT</title>
+    <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/en/4/4f/Maulana_Azad_National_Institute_of_Technology_Logo.png" type="image/x-icon" />
     <style>
         body {
             margin: 0;
@@ -46,6 +47,68 @@
             font-size: 24px;
             font-weight: bold;
         }
+
+        .form-label {
+            display: block;
+            margin-bottom: 5px;
+            font-weight: bold;
+        }
+
+        .scholar-input {
+            width: 100%;
+            padding: 5px;
+            border: 1px solid black;
+            border-radius: 5px;
+        }
+
+        .otp-section {
+            display:none;
+        }
+
+        .otpInput {
+            width: 50%;
+            padding: 8px 10px;
+            height: 25px;
+            font-size: 15px;
+            border: 1px solid black;
+            border-radius: 5px;
+            margin-bottom: 5px;
+        }
+
+        .submit_button{
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            margin-top: 5px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 15px;
+            font-weight: bold;
+            transition: background-color 0.3s ease;
+        }
+
+        .verify{
+            background-color: #4CAF50;
+            color: white;
+            padding: 5px 5px;
+            border: none;
+            margin-top: 5px;
+            margin-left: 3px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 17px;
+            font-weight: bold;
+            transition: background-color 0.3s ease;
+        }
+
+        .line{
+            display: flex;
+        }
+        .submit_button:hover {
+            background-color: #45a049;
+        }
+
     </style>
 </head>
 <body>
@@ -53,12 +116,27 @@
         <div class="logo">
             <img src="https://upload.wikimedia.org/wikipedia/en/4/4f/Maulana_Azad_National_Institute_of_Technology_Logo.png" alt="Logo">
             <h1>
-                <span>Campus Corner</span>
+                <span>myMANIT</span>
             </h1>
         </div>
 
         <form method="post">
-            inp
+            <div class="scholar-no" id="scholarSection">
+                
+                <div class="scholar_input">
+                    <label for="scholar-number" class="form-label">Scholar Number:</label>
+                    <input type="text" class="scholar-input" name="scholarNumber" id="scholarnum">
+                </div>
+                
+                <div id="otpSection" class="otp-section">
+                    <label for="otp-entry" class="form-label">OTP:</label>
+                    <input type="text" id="otpInput" class="otpInput" name="otp">
+                    <button class="verify" name="otpVerify">Verify</button>
+                </div>
+                
+                <button id="submit" class="submit_button" name="submit">Continue</button>
+            
+            </div>
         </form>
     </div>
 </body>
